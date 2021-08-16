a8395646c9c7ccfb667ab789cbb1178a08ec664b
dpudpu <dpudpu11@gmail.com>
2021-08-16 21:36:05 +0900

    chore: Add kotest dependency

--- algorithm/build.gradle.kts
+++ algorithm/build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
     testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.6.0")
+    testImplementation("io.kotest:kotest-framework-engine-jvm:4.6.0")
 }
 
 tasks.test {
