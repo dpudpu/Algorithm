415a5d45fa0780d0998638453485c63b4fc62e03
dpudpu <dpudpu11@gmail.com>
2022-01-27 22:58:27 +0900

    LeetCode0938

---
+++ algorithm/src/main/kotlin/leetcode/LeetCode0938.kt
@@ -0,0 +1,8 @@
+package leetcode
+
+fun rangeSumBST(node: TreeNode?, low: Int, high: Int): Int {
+    if (node == null) return 0
+    if (node.`val` < low) return rangeSumBST(node.right, low, high)
+    if (node.`val` > high) return rangeSumBST(node.left, low, high)
+    return rangeSumBST(node.right, low, high) + node.`val` + rangeSumBST(node.left, low, high)
+}
